189a5998f3f143900799212dba51cac0abec9722
Jakub Uhlik <jakub.uhlik@gmail.com>
2014-12-26 08:27:05 +0100

    incremented version number

--- uv_tube_unwrap.py
+++ uv_tube_unwrap.py
@@ -19,7 +19,7 @@
 bl_info = {"name": "Tube UV Unwrap",
            "description": "UV unwrap tube-like meshes (all quads, no caps, fixed number of vertices in each ring)",
            "author": "Jakub Uhlik",
-           "version": (0, 2, 4),
+           "version": (0, 2, 5),
            "blender": (2, 70, 0),
            "location": "Edit mode > Mesh > UV Unwrap... > Tube UV Unwrap",
            "warning": "",
