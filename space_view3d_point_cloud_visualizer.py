65cb9bf9fac82d74f6a0c8300e67ae1d7173e8dd
Jakub Uhlik <jakub.uhlik@gmail.com>
2019-10-02 21:54:17 +0200

    billboard real size fixed

--- space_view3d_point_cloud_visualizer.py
+++ space_view3d_point_cloud_visualizer.py
@@ -2028,7 +2028,8 @@ class PCVShaders():
         {
             fcolor = vcolor[0];
             falpha = valpha[0];
-            float s = size[0];
+            // value is diameter, i need radius
+            float s = size[0] / 2;
             
             vec4 pos = view_matrix * gl_in[0].gl_Position;
             vec2 xyloc = vec2(-1 * s, -1 * s);
@@ -2048,6 +2049,61 @@ class PCVShaders():
             EmitVertex();
             
             EndPrimitive();
+        }
+    '''
+    
+    billboard_geometry_disc = '''
+        layout (points) in;
+        // 3 * 16 = 48
+        layout (triangle_strip, max_vertices = 48) out;
+        
+        in vec4 vcolor[];
+        in float valpha[];
+        
+        uniform mat4 view_matrix;
+        uniform mat4 window_matrix;
+        
+        uniform float size[];
+        
+        out vec4 fcolor;
+        out float falpha;
+        
+        vec2 disc_coords(float radius, int step, int steps)
+        {
+            const float PI = 3.1415926535897932384626433832795;
+            float angstep = 2 * PI / steps;
+            float x = sin(step * angstep) * radius;
+            float y = cos(step * angstep) * radius;
+            return vec2(x, y);
+        }
+        
+        void main()
+        {
+            fcolor = vcolor[0];
+            falpha = valpha[0];
+            float s = size[0];
+            
+            vec4 pos = view_matrix * gl_in[0].gl_Position;
+            float r = s / 2;
+            int steps = 16;
+            
+            for(int i = 0; i < steps; i++)
+            {
+                
+                gl_Position = window_matrix * (pos);
+                EmitVertex();
+                
+                vec2 xyloc = disc_coords(r, i, steps);
+                gl_Position = window_matrix * (pos + vec4(xyloc, 0, 0));
+                EmitVertex();
+                
+                xyloc = disc_coords(r, i + 1, steps);
+                gl_Position = window_matrix * (pos + vec4(xyloc, 0, 0));
+                EmitVertex();
+                
+                EndPrimitive();
+            }
+            
         }
     '''
 
@@ -2873,6 +2929,7 @@ class PCVManager():
             
             if(not use_stored):
                 shader = GPUShader(PCVShaders.billboard_vertex, PCVShaders.billboard_fragment, geocode=PCVShaders.billboard_geometry, )
+                # shader = GPUShader(PCVShaders.billboard_vertex, PCVShaders.billboard_fragment, geocode=PCVShaders.billboard_geometry_disc, )
                 batch = batch_for_shader(shader, 'POINTS', {"position": vs[:l], "color": cs[:l], })
                 
                 if('extra' not in ci.keys()):
