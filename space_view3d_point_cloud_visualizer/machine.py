85819f05dd57ab432e38cd82669e947c98f195aa
Jakub Uhlik <jakub.uhlik@gmail.com>
2020-01-20 21:47:47 +0100

    fixed sequence handler

--- space_view3d_point_cloud_visualizer/machine.py
+++ space_view3d_point_cloud_visualizer/machine.py
@@ -1614,7 +1614,7 @@ class PCVSequence():
     initialized = False
     
     @classmethod
-    def handler(cls, scene, ):
+    def handler(cls, scene, depsgraph, ):
         cf = scene.frame_current
         for k, v in cls.cache.items():
             pcv = v['pcv']
