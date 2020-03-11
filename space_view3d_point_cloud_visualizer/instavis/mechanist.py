6f9b262a7522c226bc7077b79d44ee9cacb5aad5
Jakub Uhlik <jakub.uhlik@gmail.com>
2020-03-11 20:12:18 +0100

    split

--- space_view3d_point_cloud_visualizer/instavis/mechanist.py
+++ space_view3d_point_cloud_visualizer/instavis/mechanist.py
@@ -32,7 +32,10 @@ from gpu.types import GPUShader
 from gpu_extras.batch import batch_for_shader
 
 from .debug import debug_mode, log
+from . import generators
 
+# FIXME: fix shading on origins only shader, or which should be better make it flat color, normals are not calculated so shading has no purpose
+# TODO: maybe try to make origins float a bit above surface to be a bit more useable, moving a tiny bit towards camera may be enough
 shader_directory = os.path.realpath(os.path.join(os.path.dirname(__file__), 'shaders'))
 if(not os.path.exists(shader_directory)):
     raise OSError("Did you point me to an imaginary directory? ('{}')".format(shader_directory))
@@ -67,198 +70,6 @@ def load_shader_code(name):
     
     shader_cache[name] = {'v': vs, 'f': fs, 'g': gs, }
     return vs, fs, gs
-
-
-class PCVIVFacesSampler():
-    # shuffling points in mesh sampler is very slow, PCV display percentage won't work as expected if points are not shuffled, but for instavis is not that important
-    sampler_shuffle = False
-    # default mesh sampler point color
-    sampler_constant_color = (1.0, 0.0, 1.0, )
-    # used when mesh data is not available, like when face sampler is used, but mesh has no faces, or vertex sampler with empty mesh
-    sampler_error_color = (1.0, 0.0, 1.0, )
-    
-    def __init__(self, target, count=-1, seed=0, colorize=None, constant_color=None, use_face_area=None, use_material_factors=None, ):
-        if(colorize is None):
-            colorize = 'CONSTANT'
-        if(constant_color is None):
-            constant_color = self.sampler_constant_color
-        if(colorize == 'OBJECT_VIEWPORT_DISPLAY_COLOR'):
-            colorize = 'CONSTANT'
-            constant_color = tuple(target.color[:3])
-        if(use_material_factors is None and colorize == 'MATERIAL_VIEWPORT_DISPLAY_COLOR'):
-            use_material_factors = False
-        if(use_material_factors):
-            if(colorize == 'CONSTANT'):
-                use_material_factors = False
-        
-        me = target.data
-        if(target.type not in ('MESH', )):
-            self.vs = np.array(((0.0, 0.0, 0.0, ), ), dtype=np.float32, )
-            self.ns = np.array(((0.0, 0.0, 1.0, ), ), dtype=np.float32, )
-            self.cs = np.array((self.sampler_error_color, ), dtype=np.float32, )
-            return
-        elif(len(me.polygons) == 0):
-            # no polygons to generate from, use origin
-            self.vs = np.array(((0.0, 0.0, 0.0, ), ), dtype=np.float32, )
-            self.ns = np.array(((0.0, 0.0, 1.0, ), ), dtype=np.float32, )
-            self.cs = np.array((self.sampler_error_color, ), dtype=np.float32, )
-            return
-        if(colorize == 'MATERIAL_VIEWPORT_DISPLAY_COLOR'):
-            if(len(target.data.materials) == 0):
-                # no materials, set to constant
-                colorize = 'CONSTANT'
-                constant_color = self.sampler_error_color
-                use_material_factors = False
-            materials = target.data.materials
-            if(None in materials[:]):
-                # if there is empty slot, abort it and set to constant, checking each polygon will be slow..
-                colorize = 'CONSTANT'
-                constant_color = self.sampler_error_color
-                use_material_factors = False
-        
-        l = len(me.polygons)
-        if(count == -1):
-            count = l
-        if(count > l):
-            count = l
-        
-        np.random.seed(seed=seed, )
-        
-        centers = np.zeros((l * 3), dtype=np.float32, )
-        me.polygons.foreach_get('center', centers, )
-        centers.shape = (l, 3)
-        
-        normals = np.zeros((l * 3), dtype=np.float32, )
-        me.polygons.foreach_get('normal', normals, )
-        normals.shape = (l, 3)
-        
-        choices = np.indices((l, ), dtype=np.int, )
-        choices.shape = (l, )
-        weights = np.zeros(l, dtype=np.float32, )
-        me.polygons.foreach_get('area', weights, )
-        # make it all sum to 1.0
-        weights *= 1.0 / np.sum(weights)
-        indices = np.random.choice(choices, size=count, replace=False, p=weights, )
-        
-        if(colorize == 'MATERIAL_VIEWPORT_DISPLAY_COLOR'):
-            material_indices = np.zeros(l, dtype=np.int, )
-            me.polygons.foreach_get('material_index', material_indices, )
-            material_colors = np.zeros((len(materials), 3), dtype=np.float32, )
-            material_factors = np.zeros((len(materials)), dtype=np.float32, )
-            for i, m in enumerate(materials):
-                mc = m.diffuse_color[:3]
-                material_colors[i][0] = mc[0] ** (1 / 2.2)
-                material_colors[i][1] = mc[1] ** (1 / 2.2)
-                material_colors[i][2] = mc[2] ** (1 / 2.2)
-                material_factors[i] = m.pcv_instavis.factor
-        
-        if(use_material_factors):
-            material_weights = np.take(material_factors, material_indices, axis=0, )
-            material_weights *= 1.0 / np.sum(material_weights)
-            if(use_face_area):
-                weights = (weights + material_weights) / 2.0
-            else:
-                weights = material_weights
-            indices = np.random.choice(choices, size=count, replace=False, p=weights, )
-        
-        li = len(indices)
-        if(colorize == 'CONSTANT'):
-            colors = np.column_stack((np.full(l, constant_color[0] ** (1 / 2.2), dtype=np.float32, ),
-                                      np.full(l, constant_color[1] ** (1 / 2.2), dtype=np.float32, ),
-                                      np.full(l, constant_color[2] ** (1 / 2.2), dtype=np.float32, ), ))
-        elif(colorize == 'MATERIAL_VIEWPORT_DISPLAY_COLOR'):
-            colors = np.zeros((li, 3), dtype=np.float32, )
-            colors = np.take(material_colors, material_indices, axis=0, )
-        
-        if(l == count):
-            vs = centers
-            ns = normals
-            cs = colors
-        else:
-            vs = np.take(centers, indices, axis=0, )
-            ns = np.take(normals, indices, axis=0, )
-            cs = np.take(colors, indices, axis=0, )
-        
-        if(self.sampler_shuffle):
-            a = np.concatenate((vs, ns, cs), axis=1, )
-            np.random.shuffle(a)
-            vs = a[:, :3]
-            ns = a[:, 3:6]
-            cs = a[:, 6:]
-        
-        self.vs = vs
-        self.ns = ns
-        self.cs = cs
-
-
-class PCVIVVertsSampler():
-    # shuffling points in mesh sampler is very slow, PCV display percentage won't work as expected if points are not shuffled, but for instavis is not that important
-    sampler_shuffle = False
-    # default mesh sampler point color
-    sampler_constant_color = (1.0, 0.0, 1.0, )
-    # used when mesh data is not available, like when face sampler is used, but mesh has no faces, or vertex sampler with empty mesh
-    sampler_error_color = (1.0, 0.0, 1.0, )
-    
-    def __init__(self, target, count=-1, seed=0, constant_color=None, ):
-        if(constant_color is None):
-            constant_color = self.sampler_constant_color
-        
-        me = target.data
-        
-        if(target.type not in ('MESH', )):
-            self.vs = np.array(((0.0, 0.0, 0.0, ), ), dtype=np.float32, )
-            self.ns = np.array(((0.0, 0.0, 1.0, ), ), dtype=np.float32, )
-            self.cs = np.array((self.sampler_error_color, ), dtype=np.float32, )
-            return
-        elif(len(me.vertices) == 0):
-            # no vertices to generate from, use origin
-            self.vs = np.array(((0.0, 0.0, 0.0, ), ), dtype=np.float32, )
-            self.ns = np.array(((0.0, 0.0, 1.0, ), ), dtype=np.float32, )
-            self.cs = np.array((self.sampler_error_color, ), dtype=np.float32, )
-            return
-        
-        l = len(me.vertices)
-        if(count == -1 or count > l):
-            count = l
-        
-        locations = np.zeros((l * 3), dtype=np.float32, )
-        me.vertices.foreach_get('co', locations, )
-        locations.shape = (l, 3)
-        
-        normals = np.zeros((l * 3), dtype=np.float32, )
-        me.vertices.foreach_get('normal', normals, )
-        normals.shape = (l, 3)
-        
-        gc = [c ** (1 / 2.2) for c in constant_color]
-        colors = np.column_stack((np.full(l, gc[0], dtype=np.float32, ),
-                                  np.full(l, gc[1], dtype=np.float32, ),
-                                  np.full(l, gc[2], dtype=np.float32, ), ))
-        
-        if(l == count):
-            vs = locations
-            ns = normals
-            cs = colors
-        else:
-            # randomize points only when needed, when not using all vertices
-            choices = np.indices((l, ), dtype=np.int, )
-            choices.shape = (l, )
-            np.random.seed(seed=seed, )
-            indices = np.random.choice(choices, size=count, replace=False, )
-            
-            vs = np.take(locations, indices, axis=0, )
-            ns = np.take(normals, indices, axis=0, )
-            cs = np.take(colors, indices, axis=0, )
-        
-        if(self.sampler_shuffle):
-            a = np.concatenate((vs, ns, cs), axis=1, )
-            np.random.shuffle(a)
-            vs = a[:, :3]
-            ns = a[:, 3:6]
-            cs = a[:, 6:]
-        
-        self.vs = vs
-        self.ns = ns
-        self.cs = cs
 
 
 class PCVIVMechanist():
@@ -533,9 +344,9 @@ class PCVIVMechanist():
                     count = instance_options.max_points
                     color_constant = instance_options.color_constant
                     if(instance_options.source == 'VERTICES'):
-                        sampler = PCVIVVertsSampler(base, count=count, seed=0, constant_color=color_constant, )
+                        sampler = generators.PCVIVVertsSampler(base, count=count, seed=0, constant_color=color_constant, )
                     else:
-                        sampler = PCVIVFacesSampler(base, count=count, seed=0, colorize=instance_options.color_source, constant_color=color_constant, use_face_area=instance_options.use_face_area, use_material_factors=instance_options.use_material_factors, )
+                        sampler = generators.PCVIVFacesSampler(base, count=count, seed=0, colorize=instance_options.color_source, constant_color=color_constant, use_face_area=instance_options.use_face_area, use_material_factors=instance_options.use_material_factors, )
                     vs, ns, cs = (sampler.vs, sampler.ns, sampler.cs, )
                     
                     if(quality == 'BASIC'):
